c381725e559d806cefff566688be4d94f63d0359
Johannes Christ <jc@jchri.st>
2018-06-05 13:56:06 +0200

    Log on startup and allow reusing addresses.

--- lib/hormex.ex
+++ lib/hormex.ex
@@ -6,9 +6,9 @@ defmodule Hormex do
   Start the TCP listener on the given port.
   """
   def listen(port) do
-    options = [:binary, packet: :http, active: false]
+    options = [:binary, packet: :http, active: false, reuseaddr: true]
     {:ok, socket} = :gen_tcp.listen(port, options)
-    Logger.info "Listening on port #{port}"
+    Logger.info("Listening on port #{port}")
     Listener.accept_forever(socket)
   end
 end
