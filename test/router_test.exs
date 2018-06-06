70becb46ad733f14a6bc48d589d8bcbe02bb7880
Johannes Christ <jc@jchri.st>
2018-06-06 20:23:23 +0200

    Add a few router test cases.

---
+++ test/router_test.exs
@@ -0,0 +1,35 @@
+defmodule Hormex.RouterTest do
+  alias Hormex.Router
+  use ExUnit.Case, async: true
+
+  setup do
+    %{router: start_supervised!(Hormex.Router)}
+  end
+
+  test "`route_for` for unknown route returns `nil`", %{router: router} do
+    assert {:error, _reason} = Router.route_for(
+      router,
+      "/surely/non/existant/route/"
+    )
+  end
+
+  test "`all_routes` returns an empty map", %{router: router} do
+    assert Router.all_routes(router) == %{}
+  end
+
+  test "`add_route` properly adds a new route", %{router: router} do
+    path = "/test"
+    options = %{location: "/my/router/test/"}
+    Router.add_route(router, path, options)
+
+    assert Router.route_for(router, "/test") == {:ok, {path, options}}
+    assert Router.route_for(router, "/test/me") == {:ok, {path, options}}
+    assert Router.route_for(router, "/test/page.html") == {:ok, {path, options}}
+    assert {:error, _reason} = Router.route_for(
+      router,
+      "/surely/non/existant/route/"
+    )
+
+    assert Router.all_routes(router) == %{path => options}
+  end
+end
