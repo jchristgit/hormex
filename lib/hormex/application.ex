1b4525f8223a66ba74246dff49dc5ee56ca7f92c
Johannes Christ <jc@jchri.st>
2018-06-07 01:10:27 +0200

    Change bootup logging depending on state.

--- lib/hormex/application.ex
+++ lib/hormex/application.ex
@@ -1,8 +1,12 @@
 defmodule Hormex.Application do
+  @moduledoc false
+
   require Logger
   use Application
 
-  def start(_type, _args) do
+  @impl true
+  @spec start(Supervisor.start_type(), term) :: Supervisor.on_start()
+  def start(start_type, _args) do
     children = [
       {Task.Supervisor, name: Hormex.HandlerSupervisor},
       {Hormex.Router, name: Hormex.Router},
@@ -10,10 +14,40 @@ defmodule Hormex.Application do
     ]
 
     opts = [strategy: :one_for_one, name: Hormex.Supervisor]
-    Logger.info("Starting up main supervisor.")
-    Supervisor.start_link(children, opts)
+    link_result = Supervisor.start_link(children, opts)
+
+    case link_result do
+      {:ok, pid} ->
+        case start_type do
+          :normal ->
+            Logger.info("Started main supervisor normally, running as #{inspect(pid)}")
+
+          {:takeover, node} ->
+            Logger.info(
+              "Started main supervisor as takeover from #{inspect(node)}, running as #{
+                inspect(pid)
+              }"
+            )
+
+          {:failover, node} ->
+            Logger.info(
+              "Started main supervisor as failover from #{inspect(node)}, running as #{
+                inspect(pid)
+              }"
+            )
+        end
+
+      {:error, {:shutdown, reason}} ->
+        Logger.error("Supervisor terminating, reason #{reason}")
+
+      {:error, {:already_started, pid}} ->
+        Logger.error("Cannot start application, already running at #{inspect(pid)}")
+    end
+
+    link_result
   end
 
+  @impl true
   def stop(_state) do
     Logger.info("Stopping main supervisor.")
   end
